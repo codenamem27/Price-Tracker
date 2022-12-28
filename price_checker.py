124bed48558b5a98b1bbc514c6caaf076f049880
codenamem27 <codenamem27@users.noreply.github.com>
2022-12-28 18:41:29 +1100

    Create price_checker.py

---
+++ price_checker.py
@@ -0,0 +1,18 @@
+import datetime
+import time
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+from bs4 import BeautifulSoup
+from faker import Faker
+
+
+
+def main():
+
+  print("foobar")
+
+if __name__ == '__main__':
+    main()
+
+    
+ 
