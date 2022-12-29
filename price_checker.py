13ce63672a29d5cdd115776d49df3a16ce900985
codenamem27 <codenamem27@users.noreply.github.com>
2022-12-29 22:11:45 +1100

    Updated

--- price_checker.py
+++ price_checker.py
@@ -13,7 +13,8 @@ d1 = fake.random.randint(10, 28)
 d2 = fake.random.randint(10, 28)
 
 
-def check_amazon_item_price(playwright: Playwright, item_name: str, item_id: str, desired_price: float) -> None:
+def check_amazon_item_price(playwright: Playwright, item_name: str, item_id: str, 
+                            desired_price: float, is_gh_action: False) -> None:
 
     browser = playwright.chromium.launch(headless=True)
     ua = (
@@ -27,20 +28,47 @@ def check_amazon_item_price(playwright: Playwright, item_name: str, item_id: str
 
     # url = f"https://www.amazon.com.au/dp/B00IMF1CDO"
     url = f"https://www.amazon.com.au/dp/{item_id}"
-    page.goto(url, wait_until='domcontentloaded')
-    # aaa = page.content()
-    
-    page.locator("#nav-global-location-popover-link").click()
-    page.get_by_role("textbox", name="or enter a postcode in Australia").click()
-    page.get_by_role("textbox", name="or enter a postcode in Australia").fill("2000")
-    page.locator("#GLUXPostalCodeWithCity_DropdownButton span").nth(1).click()
-    # page.locator("#GLUXPostalCodeWithCity_DropdownList_3").click()
-    page.get_by_role("button", name="Submit").click()
 
-    page.is_visible('#corePrice_feature_div')
+    print(f"item url is: {url}")
+    page.goto(url, wait_until='domcontentloaded')
+
+    if is_gh_action:
+
+        page.locator("#nav-global-location-popover-link").click(delay=2000)
+
+        page.get_by_role("textbox", name="or enter a postcode in Australia").click()
+        page.get_by_role("textbox", name="or enter a postcode in Australia").type(text="2000", delay=10)
+
+        page.locator("#GLUXPostalCodeWithCity_DropdownList").select_option('HAYMARKET')
+        # page.select_option(selector="#GLUXPostalCodeWithCity_DropdownList", index=0)
+
+        # page.locator("a:has-text(\"HAYMARKET\")").click()
+
+        page.get_by_role("button", name="Submit").click()
+        page.wait_for_load_state(state="domcontentloaded", timeout=2000)
+        page.wait_for_selector('#qualifiedBuybox', state='attached')
+
+    # page.is_visible('#corePrice_feature_div')
+    page.wait_for_selector('#corePrice_feature_div', state='visible')
+
+    # # url = f"https://www.amazon.com.au/dp/B00IMF1CDO"
+    # url = f"https://www.amazon.com.au/dp/{item_id}"
+    # page.goto(url, wait_until='domcontentloaded')
+    # # aaa = page.content()
+    
+    # page.locator("#nav-global-location-popover-link").click()
+    # page.get_by_role("textbox", name="or enter a postcode in Australia").click()
+    # page.get_by_role("textbox", name="or enter a postcode in Australia").fill("2000")
+    # page.locator("#GLUXPostalCodeWithCity_DropdownButton span").nth(1).click()
+    # # page.locator("#GLUXPostalCodeWithCity_DropdownList_3").click()
+    # page.get_by_role("button", name="Submit").click()
+
+    # page.is_visible('#corePrice_feature_div')
 
     price_list_html = page.inner_html("#desktop_buybox")
+
     print(price_list_html)
+
     soup = BeautifulSoup(price_list_html, 'html.parser')
     #document.querySelector('#desktop_buybox .a-box-group #corePrice_feature_div .a-price.a-text-price')
 
@@ -69,10 +97,8 @@ def check_amazon_item_price(playwright: Playwright, item_name: str, item_id: str
 
 def main():
 
-  print("foobar")
-  
   with sync_playwright() as playwright:
-    check_amazon_item_price(playwright, "Wera Ratchet Small Set", "B004VMWZLU", 150)
+    check_amazon_item_price(playwright, "Wera Ratchet Small Set", "B004VMWZLU", 150, True)
 
 
 if __name__ == '__main__':
