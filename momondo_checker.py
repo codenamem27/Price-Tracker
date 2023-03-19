922e289a18b7ab4332d980e164bd8a37d6a21639
RW <codenamem27@gmail.com>
2023-03-19 23:45:31 +1100

    add Momondo checker.

--- momondo_checker.py
+++ momondo_checker.py
@@ -11,6 +11,10 @@ import logging
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+from datetime import datetime
+
+from bs4 import Tag
+
 
 fake = Faker()
 d1 = fake.random.randint(10, 28)
@@ -21,7 +25,7 @@ results = []
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s [%(levelname)s] %(message)s",
-                    handlers=[logging.FileHandler("flight_checker.log"), logging.StreamHandler()])
+                    handlers=[logging.FileHandler("momondo_checker.log"), logging.StreamHandler()])
 
 
 def check_locator_and_click(pg: Page, item_name: str):
@@ -43,6 +47,7 @@ def check_locator_and_click(pg: Page, item_name: str):
 
     return error_score
 
+
 def attach_img(msg: MIMEMultipart, image_file: str):
     fp = open(image_file, "rb")
     msg_image = MIMEImage(fp.read())
@@ -54,101 +59,80 @@ def attach_img(msg: MIMEMultipart, image_file: str):
     return msg
 
 
-def check_iwantthatflight(playwright: Playwright, items: [str]) -> None:
+def check_momondo(playwright: Playwright, all_flight_items: [str]) -> None:
 
-    browser = playwright.chromium.launch(headless=True)
+    result = []
+    email_msg = MIMEMultipart('related')
+    flight_city = "undefined"
+
+    browser = playwright.chromium.launch(headless=False)
     ua = (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/69.0.3497.100 Safari/537.36"
     )
 
-    email_msg = MIMEMultipart('related')
 
-    flight_city = ""
-
-    for idx, itm_str in enumerate(items):
-
-        error_score = 0
-        error_score_display = ""
+    for idx, flight_item_str in enumerate(all_flight_items):
 
         context = browser.new_context(user_agent=ua)
         page = context.new_page()
 
-        itm_str = str(itm_str)
-        print(itm_str)
-        flight_city_from_to = str(itm_str).split(",")
+        flight_item_data = str(flight_item_str).split(",")
+        flight_city = flight_item_data[0].strip()
+        flight_from = datetime.strptime(flight_item_data[1].strip(), "%d/%b/%Y").strftime("%Y-%m-%d")
+        flight_to = datetime.strptime(flight_item_data[2].strip(), "%d/%b/%Y").strftime("%Y-%m-%d")
+        flight_threshold = int(flight_item_data[3].strip())
 
-        flight_city = flight_city_from_to[0].strip()
-        flight_from = flight_city_from_to[1].strip()
-        flight_to = flight_city_from_to[2].strip()
-        flight_threshold = int(flight_city_from_to[3].strip())
+        d1 = fake.random.randint(10, 28)
+        d2 = fake.random.randint(10, 28)
 
-        url = f"https://fly.iwantthatflight.com.au/flight.ashx?&oc=SYD&dc={flight_city}&dd={flight_from}&rd={flight_to}&cur=AUD"
-        # url = f"https://fly.iwantthatflight.com.au/flight.ashx?&oc=SYD&dc=CPH&dd={d1}/Jun/2023&rd={d2}/Jul/2023&cur=AUD"
+        url = f"https://www.momondo.com.au/flight-search/SYD-{flight_city}/{flight_from}/{flight_to}?sort=price_a"
+        # url = f"https://www.momondo.com.au/flight-search/SYD-CPH/2023-06-{d1}/2023-07-{d2}?sort=price_a"
         print(url)
-        page.goto(url, wait_until='load')
 
-        page.wait_for_timeout(1000)
+        page.goto(url, wait_until='domcontentloaded', timeout=3000)
+        # page.wait_for_timeout(5000)
 
-        page.wait_for_selector('#msgfade', state='attached')
-        # document.querySelector('#msgfade')!=null")
-        page.wait_for_function("document.querySelector('#msgfade').hasAttribute('class')")
-        page.wait_for_function("document.querySelector('#msgfade').getAttribute('class')=='complete'", timeout=60000)
+        page.wait_for_function("document.querySelector('.skp2.skp2-inlined').getAttribute('aria-hidden')=='true'", timeout=90000)
+        results.append(f"<p style='font-size:15px; font-weight: bold;'>{flight_item_str.replace('/2023', '').replace('/', '-')}</p>")
 
-        # page.is_visible('#MessageContainer')
-        page.wait_for_selector('#MessageContainer', state='visible')
+        # price_list_html = page.inner_html(".Ui-Flights-Results-Components-ListView-container")
+        items = page.query_selector_all(".Ui-Flights-Results-Components-ListView-container div[data-resultid]")
+        for idx, flight_itm in enumerate(items):
+            if idx ==0:
+                # skip the advertisement fare
+                continue
 
-        error_score += check_locator_and_click(page, "3 Stops")
-        error_score += check_locator_and_click(page, "AirAsia X")
-        error_score += check_locator_and_click(page, "Scoot")
-        error_score += check_locator_and_click(page, "Cebu Pacific")
-        error_score += check_locator_and_click(page, "AirAsia")
-        # check_locator_and_click(page, "Ryanair")
-        error_score += check_locator_and_click(page, "Vietnam Airlines")
+            item_price = flight_itm.query_selector("div[class*='-price-text-container'] div")
+            price_value = int(item_price.inner_html().replace("$", "").replace(",", ""))
 
-        # page.pause()
-
-        price_list_html = page.inner_html("#LeaveCalender")
-        # print(price_list_html)
-        soup = BeautifulSoup(price_list_html, 'html.parser')
-        price_items = soup.select('.plan-price.PriceResult.Int_PriceResult')
-
-        if error_score > 0:
-            error_score_display = f"({str(error_score)})"
-
-        results.append(f"<p style='font-size:15px; font-weight: bold;'>{itm_str.replace('/2023', '').replace('/', '-')} {error_score_display}</p>")
-
-        for idx, price_item in enumerate(price_items):
-
-            price_value = int(str(price_item.contents[0]).replace("$", ""))
-
-            print(f"{price_value}")
             if price_value < flight_threshold:
-                print(f"threshold: {flight_threshold}")
                 results.append(f"{price_value} *****<br>")
-
-                ss_file_name = f"{flight_city}-{flight_from.replace('/', '')}-{flight_to.replace('/', '')}-{fake.random.randint(1000, 2000)}.jpg"
-                page.locator(".plan.return-plan").nth(idx).screenshot(path=ss_file_name)
-
-                results.append(f"<img src='cid:{ss_file_name}' alt='Price' ><br>")
-                email_msg = attach_img(email_msg, ss_file_name)
-
             else:
                 results.append(f"{price_value} <br>")
 
-            if idx == 1:
-                # results.append("\n")
+            print(item_price.inner_html().replace("$", "").replace(",", ""))
+            if idx == 2:
                 break
-
-        results.append(f"<p>{url}</p>")
+        results.append(f"{url}<br><br>")
 
         context.close()
-        time.sleep(2)
+        time.sleep(3)
+
+
+        # soup = BeautifulSoup(price_list_html, 'html.parser')
+        # all_items = soup.select("div[data-resultid]")
+        #
+        # first_item: Tag = all_items[0]
+        #
+        # result: Tag = first_item.select("div[class*='-price-text-container'] div")[0]
+        # print(result.next_element)
+        # page.pause()
 
     browser.close()
 
-    send_html_email(email_msg, "\n".join(results), f"Flight Checker: {flight_city} - IWTF")
+    send_html_email(email_msg, "\n".join(results), f"Flight Checker: {flight_city} - Momondo")
 
 
 def send_html_email(msg: MIMEMultipart, result_html: str, subject: str):
@@ -212,7 +196,7 @@ def main():
         print("email_mima not available!")
 
     with sync_playwright() as playwright:
-        check_iwantthatflight(playwright, flight_items)
+        check_momondo(playwright, flight_items)
 
 
 if __name__ == '__main__':
