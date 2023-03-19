7ae8c6fac5650241758561900e6b24a70af0f2bb
RW <codenamem27@gmail.com>
2023-03-19 16:05:21 +1100

    update price_checker.

--- price_checker.py
+++ price_checker.py
@@ -31,81 +31,83 @@ def check_amazon_item_price(playwright: Playwright, items: [str]) -> None:
     page = context.new_page()
 
     for idx, itm_str in enumerate(items):
-        print(itm_str)
-        item_name_id_price = str(itm_str).split("/")
+        try:
+            print(itm_str)
+            item_name_id_price = str(itm_str).split("/")
 
-        item_name = item_name_id_price[0]
-        item_id = item_name_id_price[1]
-        desired_price = float(item_name_id_price[2])
+            item_name = item_name_id_price[0]
+            item_id = item_name_id_price[1]
+            desired_price = float(item_name_id_price[2])
 
-        # url = f"https://www.amazon.com.au/dp/B00IMF1CDO"
-        url = f"https://www.amazon.com.au/dp/{item_id}"
+            # url = f"https://www.amazon.com.au/dp/B00IMF1CDO"
+            url = f"https://www.amazon.com.au/dp/{item_id}"
 
-        print(f"item url is: {url}")
-        page.goto(url, wait_until='domcontentloaded')
+            print(f"item url is: {url}")
+            page.goto(url, wait_until='domcontentloaded')
 
-        if idx == 0:
-            page.locator("#nav-global-location-popover-link").click(delay=2000)
+            if idx == 0:
+                page.locator("#nav-global-location-popover-link").click(delay=2000)
 
-            page.get_by_role("textbox", name="or enter a postcode in Australia").click(delay=1000)
-            page.get_by_role("textbox", name="or enter a postcode in Australia").type(text="2000", delay=50)
+                page.get_by_role("textbox", name="or enter a postcode in Australia").click(delay=1000)
+                page.get_by_role("textbox", name="or enter a postcode in Australia").type(text="2000", delay=50)
 
-            if page.is_enabled("#GLUXPostalCodeWithCity_DropdownList"):
-                page.locator("#GLUXPostalCodeWithCity_DropdownList").select_option('SYDNEY')
+                if page.is_enabled("#GLUXPostalCodeWithCity_DropdownList"):
+                    page.locator("#GLUXPostalCodeWithCity_DropdownList").select_option('SYDNEY')
+                else:
+                    print("** Fatal error: suburb dropdown is not enabled")
+                    time.sleep(2)
+                    page.locator("#GLUXPostalCodeWithCity_DropdownList").select_option('SYDNEY')
+
+                page.get_by_role("button", name="Submit").click(delay=1000)
+                page.wait_for_load_state(state="domcontentloaded", timeout=2000)
+
+                start_time = datetime.datetime.now().replace(microsecond=0)
+                page.wait_for_selector('#qualifiedBuybox', state='visible', timeout=20000)
+                end_time = datetime.datetime.now().replace(microsecond=0)
+                print(f"waited '#qualifiedBuybox' for: {end_time - start_time}")
+
+            page.wait_for_selector('#corePrice_feature_div', state='attached', timeout=20000)
+
+            price_list_html = page.inner_html("#desktop_buybox")
+
+            # debug
+            # print(price_list_html)
+
+            soup = BeautifulSoup(price_list_html, 'html.parser')
+
+            price_item = soup.select('#corePrice_feature_div .a-offscreen')
+            print(f"print_item: {price_item}")
+            if len(price_item) == 0:
+                price_item = soup.select('#booksHeaderInfoContainer #booksHeaderSection #price')
+
+            print(f"{item_name} - Target: {desired_price}")
+            current_price = str(price_item[0].string).removeprefix('$')
+            print(f"- {current_price}")
+
+            if float(current_price) <= desired_price:
+                print("good deal!")
+                send_email(subject=f"{item_name} - Current:{current_price}, Target:{desired_price}",
+                           body=f"{item_name}: {url}")
             else:
-                print("** Fatal error: suburb dropdown is not enabled")
-                time.sleep(2)
-                page.locator("#GLUXPostalCodeWithCity_DropdownList").select_option('SYDNEY')
+                print("still expensive :(")
 
-            page.get_by_role("button", name="Submit").click(delay=1000)
-            page.wait_for_load_state(state="domcontentloaded", timeout=2000)
+            if idx + 1 != len(items):
+                print("Wait for a few seconds before proceeding to the next item.\n\n")
+                time.sleep(5)
 
-            start_time = datetime.datetime.now().replace(microsecond=0)
-            page.wait_for_selector('#qualifiedBuybox', state='visible', timeout=20000)
-            end_time = datetime.datetime.now().replace(microsecond=0)
-            print(f"waited '#qualifiedBuybox' for: {end_time - start_time}")
-
-        page.wait_for_selector('#corePrice_feature_div', state='attached', timeout=20000)
-
-        price_list_html = page.inner_html("#desktop_buybox")
-
-        # debug
-        # print(price_list_html)
-
-        soup = BeautifulSoup(price_list_html, 'html.parser')
-
-        price_item = soup.select('#corePrice_feature_div .a-offscreen')
-        print(f"print_item: {price_item}")
-        if len(price_item) == 0:
-            price_item = soup.select('#booksHeaderInfoContainer #booksHeaderSection #price')
-
-        print(f"{item_name} - Target: {desired_price}")
-        current_price = str(price_item[0].string).removeprefix('$')
-        print(f"- {current_price}")
-
-        if float(current_price) <= desired_price:
-            print("good deal!")
-            send_email(subject=f"{item_name} - Current:{current_price}, Target:{desired_price}",
-                       body=f"{item_name}: {url}")
-        else:
-            print("still expensive :(")
-
-        if idx + 1 != len(items):
-            print("Wait for a few seconds before proceeding to the next item.\n\n")
-            time.sleep(5)
+        except Exception as ex:
+            print(f"Failed to check price:\n{ex}")
 
     context.close()
     browser.close()
 
 
-def send_email(subject, body, email="c o d e n a m e m 2 7 @ g m a i l . c o m"):
+def send_email(subject, body, email="c o d e n a m e m 2 7 # g m a i l . c o m"):
     user = "rwudev1"
     pwd = email_credential
-    recipient = email.replace(" ", "")
+    recipient = email.replace(" ", "").replace("#","@")
     FROM = "Price Checker"
     TO = recipient if isinstance(recipient, list) else [recipient]
-    # SUBJECT = subject
-    # TEXT = body
 
     message = f"Subject: {subject} \n\n{body}"
     try:
@@ -118,6 +120,7 @@ def send_email(subject, body, email="c o d e n a m e m 2 7 @ g m a i l . c o m")
         print('Email notification sent successfully.')
     except Exception as ex:
         print(f"failed to send mail with exception:\n{ex}")
+        exit(10)
 
 
 def main():
